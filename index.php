dc7aafa0688b488b3edc3cc072118d78ac6c2977
Matthew Reidsma <reidsmam@gvsu.edu>
2016-04-15 12:19:06 -0400

    Fixed variables, paths from testing to production

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+
 include('../../libs/simple_html_dom.php');
 
 // Load MeL search page
 
-book_title = $_POST['t'];
+$book_title = $_POST['t'];
 
 $url = 'http://elibrary.mel.org/search/a?searchtype=t&searcharg=' . $book_title . '&SORT=D&submit.x=0&submit.y=0';
 
